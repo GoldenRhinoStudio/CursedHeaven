1ccf1ae8ab2b9eab53dd2636cc8d353e5b4a0913
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2019-04-16 12:51:52 +0200

    implementing coin counter

--- CursedHeaven/Motor2D/j1Hud.cpp
+++ CursedHeaven/Motor2D/j1Hud.cpp
@@ -20,6 +20,11 @@ j1Hud::~j1Hud() {}
 bool j1Hud::Start()
 {
 	hud_text = App->tex->Load("gui/hud.png");
+	coin_value = 20;
+	coins = (char)coin_value;
+	coins_x = 20;
+	coins_y = 10;
+	font = App->font->Load("fonts/Pixeled.ttf");
 	
 	return true;
 }
@@ -52,6 +57,7 @@ bool j1Hud::Update(float dt)
 
 	SDL_Rect dk_profile = { 0,0,42,42 };
 	SDL_Rect bm_profile = { 42,0,43,42 };
+	SDL_Rect coins_r = { 0,42,35,13 };
 
 	if (App->entity->player_type == MAGE)
 		black_mage = true;
@@ -61,8 +67,19 @@ bool j1Hud::Update(float dt)
 	if (black_mage) App->render->Blit(hud_text, 5, 5, &bm_profile, SDL_FLIP_NONE, 1.0f, 1.0f, 0.0, pivot, pivot, false);
 	else if (dragoon_knight) App->render->Blit(hud_text, 5, 5, &dk_profile, SDL_FLIP_NONE, 1.0f, 1.0f, 0.0, pivot, pivot, false);
 
+	coin_value = App->entity->currentPlayer->score_points;
+	coins = (char)coin_value;
+
+	App->render->Blit(hud_text, 16, 135, &coins_r, SDL_FLIP_NONE, 1.0f, 1.0f, 0.0, pivot, pivot, false);
+//	App->font->Print(coins, coins_x, coins_y);
+	App->gui->CreateLabel(&labels_list, LABEL, 20, 137, font, &coins);
+
+
 	for (std::list<j1Button*>::iterator item = hud_buttons.begin(); item != hud_buttons.end(); ++item) {
 		(*item)->Draw(App->gui->buttonsScale);
+	}
+	for (std::list<j1Label*>::iterator item = labels_list.begin(); item != labels_list.end(); ++item) {
+		(*item)->Draw(1.0f, 0, 0, false);
 	}
 
 	return true;
@@ -75,6 +92,10 @@ bool j1Hud::CleanUp()
 	for (std::list<j1Button*>::iterator item = hud_buttons.begin(); item != hud_buttons.end(); ++item) {
 		(*item)->CleanUp();
 		hud_buttons.remove(*item);
+	}
+	for (std::list<j1Label*>::iterator item = labels_list.begin(); item != labels_list.end(); ++item) {
+		(*item)->CleanUp();
+		labels_list.remove(*item);
 	}
 
 	return true;

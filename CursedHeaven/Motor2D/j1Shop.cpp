c4ae700fc6f966654b69d7a258cbe9ebf75f8223
Dani Lorenzo <36265574+DLorenzoLaguno17@users.noreply.github.com>
2019-05-13 13:27:49 +0200

    solved item collision bug

--- CursedHeaven/Motor2D/j1Shop.cpp
+++ CursedHeaven/Motor2D/j1Shop.cpp
@@ -290,20 +290,20 @@ void j1Item::OnCollision(Collider* c1, Collider* c2) {
 
 				case HEART:
 					if (App->shop->heartLevel == 0){
-						App->entity->currentPlayer->totalLifePoints + 30;
-						App->entity->currentPlayer->lifePoints + 30;
+						App->entity->currentPlayer->totalLifePoints += 30;
+						App->entity->currentPlayer->lifePoints += 30;
 					}
 					else if (App->shop->heartLevel == 1) {
-						App->entity->currentPlayer->totalLifePoints + 55;
-						App->entity->currentPlayer->lifePoints + 55;
+						App->entity->currentPlayer->totalLifePoints += 55;
+						App->entity->currentPlayer->lifePoints += 55;
 					}
 					else if (App->shop->heartLevel == 2){
-						App->entity->currentPlayer->totalLifePoints + 80;
-						App->entity->currentPlayer->lifePoints + 80;
+						App->entity->currentPlayer->totalLifePoints += 80;
+						App->entity->currentPlayer->lifePoints += 80;
 					}
 
 					App->shop->heartLevel++;
-					break;						
+					break;
 
 				case ARMOUR:
 					//App->entity->currentPlayer->totalLifePoints + 25;
@@ -336,7 +336,6 @@ void j1Item::OnCollision(Collider* c1, Collider* c2) {
 					if (App->shop->potions < 3)	App->shop->potions++;
 					break;
 				}
-
 				canBeBought = false;
 			}
 		}

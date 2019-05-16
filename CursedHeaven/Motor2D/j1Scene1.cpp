7cee3fbc251c7cc053cf9fc08fa2a2335b8bb2d6
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2019-05-16 21:46:20 +0200

    map lvl1

--- CursedHeaven/Motor2D/j1Scene1.cpp
+++ CursedHeaven/Motor2D/j1Scene1.cpp
@@ -69,7 +69,7 @@ bool j1Scene1::Start()
 	{	
 		App->map->draw_with_quadtrees = true;
 		// The map is loaded
-		if (App->map->Load("Test.tmx"))
+		if (App->map->Load("greenmount.tmx"))
 		{
 			int w, h;
 			uchar* data = NULL;

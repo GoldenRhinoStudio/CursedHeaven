ddab198cec15a4719f23d02937bce2647abf6366
Èric Canela <knela96@gmail.com>
2019-06-10 17:50:13 +0200

    update

--- CursedHeaven/Motor2D/j1Slime.cpp
+++ CursedHeaven/Motor2D/j1Slime.cpp
@@ -65,7 +65,7 @@ bool j1Slime::Update(float dt, bool do_logic)
 		if (!App->entity->currentPlayer->active_Q) receivedAbilityDamage = false; 
 		
 		iPoint origin = { App->map->WorldToMap((int)position.x + colliderSize.x / 2, (int)position.y + colliderSize.y) };
-		iPoint destination = { App->map->WorldToMap((int)App->entity->currentPlayer->collider->rect.x, (int)App->entity->currentPlayer->collider->rect.y + App->entity->currentPlayer->collider->rect.h) };
+		iPoint destination = { App->map->WorldToMap((int)App->entity->currentPlayer->collider->rect.x, (int)App->entity->currentPlayer->collider->rect.y + App->entity->currentPlayer->collider->rect.h/2) };
 
 		int distance = (int)sqrt(pow(destination.x - origin.x, 2) + pow(destination.y - origin.y, 2));
 
